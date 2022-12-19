607f8220d94a1faa6be0ca2d96625707cf6cef45
Maksotov <maksot.ali13@gmail.com>
2022-12-20 00:40:05 +0600

    This is a page with animate photo and have sorting, its like best students

---
+++ bes.js
@@ -0,0 +1,65 @@
+function ubt() {
+    const arr = [98,100,101,139];
+    let x = document.getElementsByClassName("menu-text-title");
+    let y = document.getElementsByClassName("about-text");
+    document.getElementById("i1").src = './images/bekarys.jpg';
+    x[0].innerHTML = "Байболат Бекарыс";
+    y[0].innerHTML = `Тобы: <code>NIS</code> <br> ҰБТ балы: 100 + 39`;
+    x[1].innerHTML = "Фотомодель";
+    document.getElementById("i2").src = './images/mukha.jpg';
+    x[2].innerHTML = "Рабаев Мұқтар";
+    y[1].innerHTML = `Тобы: <code>КТЛ</code> <br> ҰБТ балы: 101`;
+    x[3].innerHTML = "КТЛщик";
+    document.getElementById("i3").src = './images/alimingoi.jpg';
+    x[4].innerHTML = "Мақсотов Әли";
+    y[2].innerHTML = `Тобы: <code>MFK-1</code> <br> ҰБТ балы: 100`;
+    x[5].innerHTML = "Жақсы бала";
+    document.getElementById("i4").src = './images/photo_2022-12-19_22-56-18.jpg';
+    x[6].innerHTML = "Темірқанат Жандарбек";
+    y[3].innerHTML = `Тобы: <code>MFK-2</code> <br> ҰБТ балы: 99`;
+    x[7].innerHTML = "Әзілқой";
+}
+
+function tobi() {
+        const arr = [98,100,101,139];
+    let x = document.getElementsByClassName("menu-text-title");
+    let y = document.getElementsByClassName("about-text");
+    document.getElementById("i3").src = './images/bekarys.jpg';
+    x[4].innerHTML = "Байболат Бекарыс";
+    y[2].innerHTML = `Тобы: <code>NIS</code> <br> ҰБТ балы: 100 + 39`;
+    x[5].innerHTML = "Фотомодель";
+    document.getElementById("i4").src = './images/mukha.jpg';
+    x[6].innerHTML = "Рабаев Мұқтар";
+    y[3].innerHTML = `Тобы: <code>КТЛ</code> <br> ҰБТ балы: 101`;
+    x[7].innerHTML = "КТЛщик";
+    document.getElementById("i1").src = './images/alimingoi.jpg';
+    x[0].innerHTML = "Мақсотов Әли";
+    y[0].innerHTML = `Тобы: <code>MFK-1</code> <br> ҰБТ балы: 100`;
+    x[1].innerHTML = "Жақсы бала";
+    document.getElementById("i2").src = './images/photo_2022-12-19_22-56-18.jpg';
+    x[2].innerHTML = "Темірқанат Жандарбек";
+    y[1].innerHTML = `Тобы: <code>MFK-2</code> <br> ҰБТ балы: 99`;
+    x[3].innerHTML = "Әзілқой";
+}
+
+function boi() {
+    const arr = [98,100,101,139];
+    let x = document.getElementsByClassName("menu-text-title");
+    let y = document.getElementsByClassName("about-text");
+    document.getElementById("i2").src = './images/bekarys.jpg';
+    x[2].innerHTML = "Байболат Бекарыс";
+    y[1].innerHTML = `Тобы: <code>NIS</code> <br> ҰБТ балы: 100 + 39`;
+    x[3].innerHTML = "Фотомодель";
+    document.getElementById("i3").src = './images/mukha.jpg';
+    x[4].innerHTML = "Рабаев Мұқтар";
+    y[2].innerHTML = `Тобы: <code>КТЛ</code> <br> ҰБТ балы: 101`;
+    x[5].innerHTML = "КТЛщик";
+    document.getElementById("i1").src = './images/alimingoi.jpg';
+    x[0].innerHTML = "Мақсотов Әли";
+    y[0].innerHTML = `Тобы: <code>MFK-1</code> <br> ҰБТ балы: 100`;
+    x[1].innerHTML = "Жақсы бала";
+    document.getElementById("i4").src = './images/photo_2022-12-19_22-56-18.jpg';
+    x[6].innerHTML = "Темірқанат Жандарбек";
+    y[3].innerHTML = `Тобы: <code>MFK-2</code> <br> ҰБТ балы: 99`;
+    x[7].innerHTML = "Әзілқой";
+}
---
+++ bests.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bestss.css">
+</head>
+<body>
+    <h1 id="b">Біздің үздіктер</h1>
+    <div id="b2">
+    <button class="but" onclick="ubt()">ҰБТ балы бойынша сұрыптау</button>
+    <button  class="but"  onclick="tobi()">Тобы бойынша сұрыптау</button>
+    <button  class="but"  onclick="boi()">Бойы бойынша сұрыптау</button>
+    </div>
+    <div class="u">
+    <div class="section">
+    <div class="menu-flip-card">
+        <div class="menu-flip-card-inner">
+            <div class="menu-flip-card-front">
+                <img src="./images/photo_2022-12-19_22-56-18.jpg" alt="Avatar" class="menu-image" id="i1">
+            </div>
+            <div class="menu-flip-card-back">
+                <div class="menu-text">
+                    <h3 class="menu-text-title">Темірқанат Жандарбек</h3>
+                    <p class="about-text">
+                        Тобы: <code>MFK-2</code> <br>
+                        ҰБТ балы: 99
+                    </p>
+                    <h3 class="menu-text-title">Әзілқой</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="menu-flip-card">
+        <div class="menu-flip-card-inner">
+            <div class="menu-flip-card-front">
+                <img src="./images/alimingoi.jpg" alt="Avatar" class="menu-image" id="i2">
+            </div>
+            <div class="menu-flip-card-back">
+                <div class="menu-text">
+                    <h3 class="menu-text-title">Мақсотов Әли</h3>
+                    <p class="about-text">
+                        Тобы: <code>MFK-1</code> <br>
+                        ҰБТ балы: 100
+                    </p>
+                    <h3 class="menu-text-title">Жақсы бала</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="menu-flip-card">
+        <div class="menu-flip-card-inner">
+            <div class="menu-flip-card-front">
+                <img src="./images/mukha.jpg" alt="Avatar" class="menu-image" id="i3">
+            </div>
+            <div class="menu-flip-card-back">
+                <div class="menu-text">
+                    <h3 class="menu-text-title">Рабаев Мұқтар</h3>
+                    <p class="about-text">
+                        Тобы: <code>КТЛ</code> <br>
+                        ҰБТ балы: 101
+                    </p>
+                    <h3 class="menu-text-title">КТЛщик</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<div class="section">
+    <div class="menu-flip-card">
+        <div class="menu-flip-card-inner">
+            <div class="menu-flip-card-front">
+                <img src="./images/bekarys.jpg" alt="Avatar" class="menu-image" id="i4">
+            </div>
+            <div class="menu-flip-card-back">
+                <div class="menu-text">
+                    <h3 class="menu-text-title">Байболат Бекарыс</h3>
+                    <p class="about-text">
+                        Тобы: <code>NIS</code> <br>
+                        ҰБТ балы: 100 + 39
+                    </p>
+                    <h3 class="menu-text-title">Фотомодель</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<script src="bes.js"></script>
+</body>
+</html>
