4ee46e714f912d2c4b6f7f8779fe503240b9cee0
Mukhtar Rabayev <210103003@stu.sdu.edu.kz>
2022-12-19 18:44:30 +0600

    Mukhtar Contacts page bitti

---
+++ contacts.php
@@ -0,0 +1,137 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="contacts.css">
+  <link rel="stylesheet" href="contactsmedia.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.cdnfonts.com/css/gotham" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="https://dostyq.online/_nuxt/b24017d0fc470abbf8fe439ea6ba05a7.svg">
+
+  <script src="videocourses.js"></script>
+
+  <title>Contacts</title>
+</head>
+<body>
+  
+
+    <div class="header">
+      <div class="tochki">
+        <img class="tochki-image" src="marathonphotos/tochki.svg" alt="" onclick="myFunction()">
+      </div>
+      <div class="dostyk-icon">
+        <img class="dostyqsvg" src="icons/dostyq.svg" alt="">
+      </div>
+      <div class="dostyq-online">
+        <a href="Main.php" class="dostyq-header">dostyq.online</a>
+      </div>    
+    <div class="pagestiniwindegipages">
+      <div class="pages">
+        <img src="./ImagesZhan/loreg.png" alt="logo1" id="logreg1">
+        <a href="LogReg.php">Вход</a>
+      </div> 
+      <div class="pages">
+        <img src="./ImagesZhan/video.png" alt="logo2" id="video1">
+        <a href="videocourses.php">Видеокурсы</a> 
+      </div>
+      <div class="pages">
+        <img src="./ImagesZhan/Марафоны.png" alt="logo3" id="marafon">
+      <a href="marathon.php">Марафоны</a>
+      </div> 
+      <div class="pages">
+        <img src="./ImagesZhan/prof.png" alt="logo4" id="prof">
+      <a href="Prof_Page.php">Профориентация</a> 
+      </div>
+      <div class="pages">
+        <img src="./ImagesZhan/kontact.png" alt="logo4" id="kontact">
+      <a href="contacts.php">Контакты</a> 
+      </div>
+    </div> 
+  </div>
+</div> 
+  <main>
+
+    <div class="main-vverh">
+      <div class="videokursy-main-text">
+        О КОМПАНИИ DOSTYQ BILIM
+        </div>
+        <div class="videokursy-opisanie">
+          
+          <p>Образовательная компания Dostyq Bilim была основана в 2002 году. С этого времени в Dostyq обучилось десятки тысяч учеников получивших гранты на обучение. На данный момент филиалы Dostyq функционируют 21 филиала в 18 городах Казахстана.</p>
+          <p>Основными направлениями являются подготовка к сдаче ЕНТ и поступлению в школы НИШ, БИЛ.</p>
+        </div>
+        
+    </div>
+
+    <div class="consult">
+      <div class="consult-cont">
+        <div>
+        <div class="main-consult-text">
+          <p>Есть вопросы или не знаете с чего начать?</p>
+        </div>
+        <div class="opisanie-consult">
+          <p>Приходите на консультацию, тепло встретим</p>
+        </div>
+        </div>
+        
+      
+        <div class="info-consult">
+          <p>ТОО "КАЗАХСТАНСКИЙ ОБРАЗОВАТЕЛЬНЫЙ ЦЕНТР "ДОСТЫК"</p>
+          <p>БИН: 060440007379
+            <br>
+            Адрес: Алматы, Микрорайон 4, дом 11</p>
+            <p>+7 707 422 19 19</p>
+        </div>
+        <!-- <div class="contacts-photo">
+          <img class="devushka-photo" src="https://dostyq.online/_nuxt/img/hoodie-girl.f547c21.png" alt="">
+        </div> -->
+        </div>
+      
+    </div>
+
+  <div class="footer">
+    <div class="left-footer">
+      <div class="dostyq-image-container">
+        <img class="dostyq-image-container-icon" src="icons/dostyq.svg" alt="">
+      </div>
+  
+      <div class="left-footer-right">
+        <p class="dostyq">dostyq.online</p>
+        <p class="svyazh">Свяжитесь с нами</p>
+        <p class="dostyq-nomer">+7 707 422 19 19</p>
+      </div>
+    </div>
+    <div class="right-footer">
+      <div>
+        <p class="weinmedia">Мы в социальных сетях</p>
+      </div>
+      <div class="socset">
+        <img src="icons/whatsapp.svg" alt="">
+        <img src="icons/youtube.svg" alt="">
+        <img src="icons/instagram.svg" alt="">
+      </div>
+    </div>
+  </div>
+  
+  <div class="after-footer">
+    <div>
+      <p class="after-footer-text">© 2022 Dostyq Bilim. Все права защищены.</p>
+    </div>
+    <div class="right-after-footer">
+      <div>
+        <p class="after-footer-text">Политика конфиденциальности</p>
+      </div>
+      <div>
+        <p class="after-footer-text">
+          Договор оферты
+        </p>
+      </div>
+    </div>
+  </div>
+  
+  </main>
+  </body>
+  </html>
